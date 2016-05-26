2849db5e69176b26b40535ffeb070a59f6acf3fb
Lodifice <Richard.Moerbitz@mailbox.tu-dresden.de>
2016-05-26 09:28:57 +0200

    New prototypes in headers

--- inc/board.h
+++ inc/board.h
@@ -13,7 +13,7 @@ int out_of_bounds(unsigned x, unsigned y);
 struct entity *collision(unsigned x, unsigned y);
 const struct board *generate_board(unsigned width, unsigned height);
 
-int add_entity(struct entity *e);
+int place_entity(struct entity *e);
 int move_entity(struct entity *e, enum direction dir);
 int remove_entity(struct entity *e);
 
