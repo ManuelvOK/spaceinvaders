bc59425cc3bfdb9807224adadee0445a3c7fa6b0
Lodifice <Richard.Moerbitz@mailbox.tu-dresden.de>
2016-06-02 10:22:07 +0200

    Improved interface between gamestate and board

--- inc/game.h
+++ inc/game.h
@@ -1,6 +1,9 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include <stdio.h>
+
+#include <board.h>
 #include <entities.h>
 
 struct game_state {
@@ -9,6 +12,10 @@ struct game_state {
     struct entity_list *lasers;
     struct entity_list **fighters;
     unsigned n_fighter_rows;
+    const struct board *the_board;
 };
+
+const struct game_state *load_state(FILE *savestate);
+const struct game_state *new_state(void);
 
 #endif /* ifndef GAME_H */
